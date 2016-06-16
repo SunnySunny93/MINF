fd7eb8885b63e90f797b7d8adaa3f4684c0723a4
SunnySunny93 <an.bo@live.de>
2016-06-16 13:56:51 +0200

    Pixelfarbe angleichen
    Dunkle Pixel werden schon besser angeglichen

--- Aufgabe 1/Aufgabe1.py
+++ Aufgabe 1/Aufgabe1.py
@@ -16,29 +16,32 @@ ausgangsbild = "cleaned.png"
 def clean(eingangsbild, ausgangsbild):
     bild = np.array(Image.open(eingangsbild))
     bildBW = np.array(Image.open(eingangsbild).convert("L"))
-    findeRGB(bild, bildBW)
-    plt.imshow(bild) 
-    plt.imsave(ausgangsbild, bild)
+    ermittelGrau(bildBW, findeRGB(bild, bildBW))
+    plt.imshow(bildBW, cmap=cm.gray) 
+    plt.imsave(ausgangsbild, bildBW, cmap =cm.gray)
     #plt.imshow(bildBW, cmap=cm.gray)
     #print(bild[1])
-    
+   
 def findeRGB(bild,bildBW):
     height, width = bildBW.shape
+    L = []
     for y in range(height):
         for x in range(width):
             pixel=bild[y,x]
             if(pixel[0] != pixel[1] or pixel[0] != pixel[2] or pixel[1] != pixel[2]): #Alle Pixel herausfinden, die nicht Grau sind.
-                bild[y,x]= [255,0,0, 255]
-                """#pixel1 = bildBW[y,x]
-                pixel2 = bildBW[y-1, x-1]
-                #pixel3 = bildBW[y+1, x+1]
-                pixel4 = bildBW[y, x-1]
-                #pixel5 = bildBW[y, x+1]
-                pixel6 = bildBW[y-1, x]
-                #pixel7 = bildBW[y+1, x]
-                #pixel8 = bildBW[y-1, x+1]
-                #pixel9 = bildBW[y+1, x-1]
-                durchschnitt = (pixel2 + pixel4 + pixel6 )/3
-                bild[y,x]=durchschnitt"""
-    #plt.imshow(bild)        
+                bildBW[y,x]= 0
+                L = L + [[y,x]]            
+    return L
+def ermittelGrau(bildBW, L):
+    grau=0
+    werte=0
+    for i in range(len(L)):
+        y,x = L[i]
+        for i in range(-1,1):
+            for j in range(-1,1):
+                if bildBW[y+i,x+j] != 0:
+                    grau = grau + bildBW[y+i,x+j]
+                    werte = werte +1
+        bildBW[y,x]=(grau/werte)
+    return bildBW
 clean(eingangsbild, ausgangsbild)
